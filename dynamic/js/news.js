4bb7a90c11e987af37d630269917bfc1f2a24021
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-03-01 19:10:30 +0200

    used axios to access json file

--- dynamic/js/news.js
+++ dynamic/js/news.js
@@ -1,8 +1,11 @@
 ﻿
-const loadNews = (path) => {
-    fetch(path)
-        .then(res => res.json())
-        .then(data => {
+
+const loadNews_version_2 = (path) => {
+    // заміняє fetch(path).then(res => res.json())
+    axios.get(path)
+        .then(resp => {
+            const { data, status } = resp;
+
             const list_news = document.getElementById("list_news");
             const side_news = document.getElementById("side_news");
             data.forEach(item => {
@@ -27,3 +30,32 @@ const loadNews = (path) => {
             });
         });
 }
+
+//const loadNews = (path) => {
+//    fetch(path)
+//        .then(res => res.json())
+//        .then(data => {
+//            const list_news = document.getElementById("list_news");
+//            const side_news = document.getElementById("side_news");
+//            data.forEach(item => {
+//                if (item.photo != "") {
+//                    list_news.innerHTML += `
+//                <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6 ">
+//                    <img loading="lazy"class="col-12" src="${item.photo}" alt="news" />
+//                    <h5 class="goods-title__title">${item.text}</h5>
+//                    <p class="additional-text">${item.published} &nbsp &nbsp<i class="fa-solid fa-eye"></i> ${Math.floor(Math.random() * (10000 - 500)) + 500}</p>
+//                </div>
+//                `;
+//                }
+//                else {
+//                    side_news.innerHTML += `
+//                    <div>
+//                        <h5>${item.text}</h5>
+//                        <p class="additional-text">${item.published} &nbsp &nbsp<i class="fa-solid fa-eye"></i>${Math.floor(Math.random() * (10000 - 500)) + 500}</p>
+//                    <hr />
+//                    </div>
+//                    `
+//                }
+//            });
+//        });
+//}
