e58dfa18fb60039ed79d45da46847c9c61cab9b2
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-03-01 17:20:39 +0200

    Add_html_hm_news_file_parser

---
+++ dynamic/js/news.js
@@ -0,0 +1,29 @@
+﻿
+const loadNews = (path) => {
+    fetch(path)
+        .then(res => res.json())
+        .then(data => {
+            const list_news = document.getElementById("list_news");
+            const side_news = document.getElementById("side_news");
+            data.forEach(item => {
+                if (item.photo != "") {
+                    list_news.innerHTML += `
+                <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6 ">
+                    <img loading="lazy"class="col-12" src="${item.photo}" alt="news" />
+                    <h5 class="goods-title__title">${item.text}</h5>
+                    <p class="additional-text">${item.published} &nbsp &nbsp<i class="fa-solid fa-eye"></i> ${Math.floor(Math.random() * (10000 - 500)) + 500}</p>
+                </div>
+                `;
+                }
+                else {
+                    side_news.innerHTML += `
+                    <div>
+                        <h5>${item.text}</h5>
+                        <p class="additional-text">${item.published} &nbsp &nbsp<i class="fa-solid fa-eye"></i>${Math.floor(Math.random() * (10000 - 500)) + 500}</p>
+                    <hr />
+                    </div>
+                    `
+                }
+            });
+        });
+}
---
+++ js/site.js
@@ -0,0 +1,8 @@
+﻿const loadDOM = (path) => {
+    //уміє загружати дом
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", path, false); //загрузка буде проведена синхронно
+    xhr.send();
+    document.write(xhr.response); //записуємо результат роботи html-розмітку
+    // тут document = body
+}
